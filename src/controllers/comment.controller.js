58396d66dcca81c7b3bf79146452c8fbdfd9883e
codeTukai <tukaighosh753@gmail.com>
2026-05-30 23:38:55 +0530

    commit comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,13 +1,31 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import mongoose from "mongoose"
+import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
-import {User} from "../models/user.models.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
-import jwt from "jsonwebtoken"
-import mongoose from 'mongoose'
+import {asyncHandler} from "../utils/asyncHandler.js"
 
-const commentController = asyncHandler(async()=>{
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
 
 })
 
-export {};
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+    }
