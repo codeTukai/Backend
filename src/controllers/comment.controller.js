138cc02f5ab98765d4822927848f06405641524a
codeTukai <tukaighosh753@gmail.com>
2026-06-06 23:52:27 +0530

    fix: implement getVideoComments function to fetch comments with pagination and user details

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,15 +1,81 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
-})
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+    const skip = (Number(page) - 1) * Number(limit);
+
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $skip: skip
+        },
+        {
+            $limit: Number(limit)
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        }
+    ]);
+
+    const totalComments = await Comment.countDocuments({
+        video: videoId
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                comments,
+                currentPage: Number(page),
+                totalPages: Math.ceil(totalComments / Number(limit)),
+                totalComments
+            },
+            "Comments fetched successfully"
+        )
+    );
+});
+
+export { getVideoComments };
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
