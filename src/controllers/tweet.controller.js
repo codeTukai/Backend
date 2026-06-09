0f849c2b098aeeef1f90feabf3eb0853c092e2f8
codeTukai <tukaighosh753@gmail.com>
2026-06-10 00:05:35 +0530

    console

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -42,6 +42,8 @@ const getUserTweets = asyncHandler(async (req, res) => {
     const tweets = await Tweet.find({ owner: userId })
         .sort({ createdAt: -1 });
 
+        console.log(tweets)
+
     return res.status(200).json(
         new ApiResponse(
             200,
