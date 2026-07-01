2b8889e09a43e770230180a0c9872c948b231837
codeTukai <tukaighosh753@gmail.com>
2026-07-01 10:20:41 +0530

    subscriptions model update

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,12 +3,12 @@ import mongoose, { Schema } from "mongoose"
 const subscriptionSchema = new Schema(
     {
         subscriber: {
-            type: Schema.Types.ObjectId, //one who is subscribing (user)
+            type: Schema.Types.ObjectId, //one who is subscribe (user) the channel
             ref:"User"
             
         },
         channel:{
-            type: Schema.Types.ObjectId, //subscriber
+            type: Schema.Types.ObjectId, //whom is subscribed by user
             ref: "User"
         }
         
